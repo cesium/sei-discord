beda1b159aa3de4695627d9b8edb1b6ff8eecffe
jtexeira <a85504@alunos.uminho.pt>
2020-12-29 18:11:51 +0000

    Bot base files with member join handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod handler;
+
+use handler::Handler;
+use serenity::prelude::*;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
