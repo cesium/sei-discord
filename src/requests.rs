9a619f1acd45c5880684e70bd1f1e58a01512cab
jtexeira <38398296+jtexeira@users.noreply.github.com>
2021-02-07 14:15:45 +0000

    Add structs to support safira interaction (#4)
    * Add safira requests structs
    
    * Add code to login on safira

---
+++ src/requests.rs
@@ -0,0 +1,33 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct LoginReply {
+    pub jwt: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AssociationRequest {
+    pub discord_association_code: String,
+    pub discord_id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "association")]
+#[serde(rename_all = "camelCase")]
+pub enum UserType {
+    Staff,
+    Empresa,
+    Participante,
+    Orador,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ErrorReply {
+    pub error: String,
+}
