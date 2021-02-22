f7d28bc745f2907d926f4bde6f76579e64d6835c
Hitler <a85504@alunos.uminho.pt>
2021-02-22 11:11:38 +0000

    Add cors support

--- src/api.rs
+++ src/api.rs
@@ -113,10 +113,37 @@ pub async fn company_vc(
     None
 }
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct CORS();
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 pub fn main(arc: Arc<CacheAndHttp>) {
     tokio::spawn(async {
         if let Err(why) = rocket::ignite()
             .mount("/", routes![spotlight_start, spotlight_end, company_vc])
+            .attach(CORS())
             .manage(arc)
             .launch()
             .await
